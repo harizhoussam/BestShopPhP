f4d6b082ea724a61f8d13d7061c108f1c1454fa0
harizhoussam <84939233+harizhoussam@users.noreply.github.com>
2021-05-29 02:31:14 +0300

    Update MarketSignUpData.php

--- MarketSignUpData.php
+++ MarketSignUpData.php
@@ -1,36 +1,36 @@
 <?php
-   $con=mysqli_connect("localhost","root","houssam","firstdatabase");
-   if (mysqli_connect_errno($con)) {
-      echo "Failed to connect to MySQL: " . mysqli_connect_error();
-   }
-	
-   $username = $_POST['username'];
-   $password = $_POST['password'];
-   $fullname = $_POST['fullname'];
-   
-   $hashedPass= password_hash($password, PASSWORD_DEFAULT);
+$con = mysqli_connect("localhost", "root", "houssam", "firstdatabase");
+if (mysqli_connect_errno($con)) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
-	if (!empty($_POST['fullname'])  && !empty($_POST['username']) && !empty($_POST['password']))
-	{
-		$checkUniqueInUsers = mysqli_query($con,"SELECT * FROM users where username='$username'");
+$username = $_POST['username'];
+$password = $_POST['password'];
+$hashedPass= password_hash($password, PASSWORD_DEFAULT);
+
+if (!empty($_POST['username']) && !empty($_POST['password'])) {
+	
+    $checkUniqueInUsers = mysqli_query($con,"SELECT * FROM users where username='$username'");
     $checkUniqueInMarkets = mysqli_query($con,"SELECT * FROM markets where username='$username'");
 	
     if (mysqli_num_rows($checkUniqueInUsers) != 0 || mysqli_num_rows($checkUniqueInMarkets) != 0) {
-				echo "Username taken, please choose new one"; }
-			else{
-				
-			   $result = mysqli_query($con,"INSERT INTO users (fullname, username, password) VALUES 
-			   ('$fullname','$username','$hashedPass')");
-			   
-				if ($result) {
-						echo "SignUp Successful";
-				} else echo "SignUp Failed, Try Again Later";
-					
-				} 
-	}else echo "Please Enter All Information";
-		
-   mysqli_close($con);
-   
+        echo "Username taken, please choose new one";
+    } else {
+        $result = mysqli_query(
+            $con,
+            "INSERT INTO markets (username, password) VALUES ('$username','$hashedPass')");
+
+        if ($result) {
+            echo "SignUp Successful";
+        } else {
+            echo "SignUp Failed, Try Again Later";
+        }
+    }
+} else {
+    echo "Please Enter All Information";
+}
+
+mysqli_close($con);
 ?>
 
 
