205db08f4f14e8a074c2b7f59b50b080c1304660
harizhoussam <84939233+harizhoussam@users.noreply.github.com>
2021-05-29 02:29:13 +0300

    Update LoginData.php

--- LoginData.php
+++ LoginData.php
@@ -6,11 +6,21 @@
 	
    $username = $_POST['username'];
    $password = $_POST['password'];
-   $result = mysqli_query($con,"SELECT * FROM users where username='$username' and password='$password'");
+   $result = mysqli_query($con,"SELECT * FROM users where username='$username'");
    
-    if (mysqli_num_rows($result) != 0) {
-	echo "True"; }
-    else echo "Wrong credentials";
+   if (mysqli_num_rows($result) != 0){
+   $num = mysqli_num_rows($result);
+   
+   if($num ==1){
+	   $row= mysqli_fetch_assoc($result);
+	   if (password_verify($password, $row['password']))
+	   echo "True";
+	   else echo "Wrong Password";
+	   
+   }else echo "Error: Multiple Users With This Username Exist";
+   
+   }
+    else echo "This Username Doesn't Exist";
 	
    mysqli_close($con);
    
