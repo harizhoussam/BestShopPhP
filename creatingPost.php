9b889aff25c5ece5d4aa74f0af5b3ab67a70193d
harizhoussam <84939233+harizhoussam@users.noreply.github.com>
2021-05-29 03:04:07 +0300

    Update creatingPost.php

--- creatingPost.php
+++ creatingPost.php
@@ -11,19 +11,20 @@
  $Latitude=$_POST['latitude'];
 
    
-  $GetOldIdSQL ="SELECT id FROM posts ORDER BY id ASC";
+   $GetOldIdSQL ="SELECT id FROM marketposts ORDER BY id DESC";
  
  $Query = mysqli_query($con,$GetOldIdSQL);
  
- while($row = mysqli_fetch_array($Query)){
- $DefaultId = $row['id']; }
+$row = mysqli_fetch_array($Query);
+
+ $DefaultId = $row['id']; 
  $file = "$DefaultId.png";
   
-  $result = mysqli_query($con,"INSERT INTO posts (id, username, text, image, longitude, latitude) VALUES ($DefaultId + 1,'$Username','$PostText','$DefaultId.png','$Longitude', '$Latitude')");
+  $result = mysqli_query($con,"INSERT INTO marketposts (id, username, text, image, longitude, latitude) VALUES ($DefaultId + 1,'$Username','$PostText','$DefaultId.png','$Longitude', '$Latitude')");
    
    if ($result) {
-	   //file_put_contents($file,base64_decode($ImageData));
-	   file_put_contents("PostsImages/UserPosts/$file",base64_decode($ImageData));
+
+	   file_put_contents("PostsImages/MarketPosts/$file",base64_decode($ImageData));
             echo "Posted";
         } else echo "Post Failed, Try Again Later";
   
